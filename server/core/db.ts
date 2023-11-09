fa97ae928e8f2f7b2d1172cb9ad67ec977f1c761
Levko Aleksandr <alex17al@mail.ru>
2023-11-09 18:13:03 +0300

    1

--- server/core/db.ts
+++ server/core/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 mongoose.Promise = Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sportHelper', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongo:27017/sportHelper', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
