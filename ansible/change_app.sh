bee03489e3229fc65e0765901c3aef12bbb27745
Levko Aleksandr <alex17al@mail.ru>
2023-11-23 23:02:56 +0300

    backups

---
+++ ansible/change_app.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Предложение выбора хоста
+echo "Выберите хост для переключения backend:"
+echo -e "\n1. backend1"
+echo -e "\n2. backend2"
+
+# Чтение ввода пользователя
+read -p "Введите номер хоста (1 или 2): " choice
+
+# Проверка выбора
+case "$choice" in
+  1)
+    backend="backend1"
+    ;;
+  2)
+    backend="backend2"
+    ;;
+  *)
+    echo "Неверный выбор. Выберите 1 или 2."
+    exit 1
+    ;;
+esac
+
+echo -e "\nПерезапускаю nginx для использования $backend\n"
+ansible-playbook nginx.yml --tags=nginx_cfg -e "backend_name=$backend"
+echo -e "\nПерезапускаю sports-helper.service для использования $backend\n"
+ansible-playbook node.yml -l $backend --tags=restart
