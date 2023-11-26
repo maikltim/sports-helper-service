31aef9e1941637c8f3be94d9dba9401b3b01122e
Levko Aleksandr <alex17al@mail.ru>
2023-11-26 18:14:05 +0300

    1

--- ansible/change_app.sh
+++ ansible/change_app.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Предложение выбора хоста
-echo "Выберите хост для переключения backend:"
-echo -e "\n1. backend1"
-echo -e "\n2. backend2"
+# Если аргументы не заданы, предложить выбор хоста
+if [ "$#" -eq 0 ]; then
+    echo "Выберите хост для переключения backend:"
+    echo -e "\n1. backend1"
+    echo -e "\n2. backend2"
 
-# Чтение ввода пользователя
-read -p "Введите номер хоста (1 или 2): " choice
+    # Чтение ввода пользователя
+    read -p "Введите номер хоста (1 или 2): " choice
+
+# Если аргументы заданы, использовать их
+else
+    choice="$1"
+fi
 
 # Проверка выбора
 case "$choice" in
@@ -21,6 +27,9 @@ case "$choice" in
     exit 1
     ;;
 esac
+
+# Вывод выбранного бэкэнда
+echo "Выбран backend: $backend"
 
 echo -e "\nПерезапускаю nginx для использования $backend\n"
 ansible-playbook nginx.yml --tags=nginx_cfg -e "backend_name=$backend"
